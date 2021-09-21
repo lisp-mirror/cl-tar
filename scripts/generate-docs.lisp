3b83d0fd0ceb6a19ecda5c6ed58cd05dc05d30f8
Eric Timmons <etimmons@mit.edu>
2021-09-21 14:47:46 -0400

    Fix doc script

--- scripts/generate-docs.lisp
+++ scripts/generate-docs.lisp
@@ -1,7 +1,7 @@
 (require "asdf")
 
-(asdf:load-system :tar)
+(asdf:load-system :tar-docs)
 
 (asdf:load-system :40ants-doc-full)
 
-(40ants-doc/builder:update-asdf-system-html-docs tar::@manual :tar)
+(40ants-doc/builder:update-asdf-system-html-docs tar-docs::@manual :tar)
