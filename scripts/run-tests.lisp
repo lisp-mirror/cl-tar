03c52eef909fade735abf75c6749221df8c0b590
Eric Timmons <etimmons@mit.edu>
2021-12-27 17:17:41 -0500

    Run create tests on CI

--- scripts/run-tests.lisp
+++ scripts/run-tests.lisp
@@ -13,5 +13,8 @@
 (unless (null (nth-value 1 (ignore-errors (asdf:test-system :tar-extract))))
   (setf *failed-p* t))
 
+(unless (null (nth-value 1 (ignore-errors (asdf:test-system :tar-create))))
+  (setf *failed-p* t))
+
 (when *failed-p*
   (uiop:quit 1))
