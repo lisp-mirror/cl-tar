278edd7cb5adfbd47356a61d66ad3fdb15543326
Eric Timmons <etimmons@mit.edu>
2022-01-03 17:26:05 -0500

    Actually skip tests on ABCL

--- test/write.lisp
+++ test/write.lisp
@@ -17,46 +17,47 @@
 
 (para:define-test write-ustar-gzip
   (para:skip-on (abcl) "decompressing stream doesn't work on ABCL."
-    (uiop:with-temporary-file (:stream s :pathname pn
-                               :type "tar.gz"
-                               :element-type '(unsigned-byte 8))
-      (tar:with-open-archive (a s :direction :output :type 'tar:ustar-archive)
-        (write-a.txt a)
-        (write-a-symlink.txt a)
-        (write-a-hardlink.txt a)
-        (write-fifo a)
-        (write-sda1 a)
-        (write-tty0 a))
-      :close-stream
-      (tar:with-open-archive (a pn)
-        (let (entry)
-          ;; First entry is a plain file, with the contents "Hello, world!"
-          (setf entry (tar:read-entry a))
-          (read-a.txt :ustar entry)
+    (para:finish
+     (uiop:with-temporary-file (:stream s :pathname pn
+                                :type "tar.gz"
+                                :element-type '(unsigned-byte 8))
+       (tar:with-open-archive (a s :direction :output :type 'tar:ustar-archive)
+         (write-a.txt a)
+         (write-a-symlink.txt a)
+         (write-a-hardlink.txt a)
+         (write-fifo a)
+         (write-sda1 a)
+         (write-tty0 a))
+       :close-stream
+       (tar:with-open-archive (a pn)
+         (let (entry)
+           ;; First entry is a plain file, with the contents "Hello, world!"
+           (setf entry (tar:read-entry a))
+           (read-a.txt :ustar entry)
 
-          ;; Next is the a-symlink.txt -> a.txt symlink
-          (setf entry (tar:read-entry a))
-          (read-a-symlink.txt :ustar entry)
+           ;; Next is the a-symlink.txt -> a.txt symlink
+           (setf entry (tar:read-entry a))
+           (read-a-symlink.txt :ustar entry)
 
-          ;; Next is the a-hardlink.txt -> a.txt hardlink
-          (setf entry (tar:read-entry a))
-          (read-a-hardlink.txt :ustar entry)
+           ;; Next is the a-hardlink.txt -> a.txt hardlink
+           (setf entry (tar:read-entry a))
+           (read-a-hardlink.txt :ustar entry)
 
-          ;; fifo
-          (setf entry (tar:read-entry a))
-          (read-fifo :ustar entry)
+           ;; fifo
+           (setf entry (tar:read-entry a))
+           (read-fifo :ustar entry)
 
-          ;; sda1
-          (setf entry (tar:read-entry a))
-          (read-sda1 :ustar entry)
+           ;; sda1
+           (setf entry (tar:read-entry a))
+           (read-sda1 :ustar entry)
 
-          ;; tty0
-          (setf entry (tar:read-entry a))
-          (read-tty0 :ustar entry)
+           ;; tty0
+           (setf entry (tar:read-entry a))
+           (read-tty0 :ustar entry)
 
-          ;; End of tar-file
-          (setf entry (tar:read-entry a))
-          (para:true (null entry)))))))
+           ;; End of tar-file
+           (setf entry (tar:read-entry a))
+           (para:true (null entry))))))))
 
 (para:define-test write-pax
   (uiop:with-temporary-file (:stream s :pathname pn
@@ -75,40 +76,41 @@
 
 (para:define-test write-pax-gzip
   (para:skip-on (abcl) "decompressing stream doesn't work on ABCL."
-    (uiop:with-temporary-file (:stream s :pathname pn
-                               :type "tar.gz"
-                               :element-type '(unsigned-byte 8))
-      (tar:with-open-archive (a s :direction :output :type 'tar:pax-archive)
-        (write-a.txt a)
-        (write-a-symlink.txt a)
-        (write-a-hardlink.txt a)
-        (write-fifo a)
-        (write-sda1 a)
-        (write-tty0 a))
-      :close-stream
-      (tar:with-open-archive (a pn)
-        (let (entry)
-          (setf entry (tar:read-entry a))
-          (read-a.txt :pax entry)
+    (para:finish
+     (uiop:with-temporary-file (:stream s :pathname pn
+                                :type "tar.gz"
+                                :element-type '(unsigned-byte 8))
+       (tar:with-open-archive (a s :direction :output :type 'tar:pax-archive)
+         (write-a.txt a)
+         (write-a-symlink.txt a)
+         (write-a-hardlink.txt a)
+         (write-fifo a)
+         (write-sda1 a)
+         (write-tty0 a))
+       :close-stream
+       (tar:with-open-archive (a pn)
+         (let (entry)
+           (setf entry (tar:read-entry a))
+           (read-a.txt :pax entry)
 
-          (setf entry (tar:read-entry a))
-          (read-a-symlink.txt :pax entry)
+           (setf entry (tar:read-entry a))
+           (read-a-symlink.txt :pax entry)
 
-          (setf entry (tar:read-entry a))
-          (read-a-hardlink.txt :pax entry)
+           (setf entry (tar:read-entry a))
+           (read-a-hardlink.txt :pax entry)
 
-          (setf entry (tar:read-entry a))
-          (read-fifo :pax entry)
+           (setf entry (tar:read-entry a))
+           (read-fifo :pax entry)
 
-          (setf entry (tar:read-entry a))
-          (read-sda1 :pax entry)
+           (setf entry (tar:read-entry a))
+           (read-sda1 :pax entry)
 
-          (setf entry (tar:read-entry a))
-          (read-tty0 :pax entry)
+           (setf entry (tar:read-entry a))
+           (read-tty0 :pax entry)
 
-          ;; End of tar-file
-          (setf entry (tar:read-entry a))
-          (para:true (null entry)))))))
+           ;; End of tar-file
+           (setf entry (tar:read-entry a))
+           (para:true (null entry))))))))
 
 (para:define-test write-v7
   (uiop:with-temporary-file (:stream s :pathname pn
@@ -124,27 +126,28 @@
 
 (para:define-test write-v7-gzip
   (para:skip-on (abcl) "decompressing stream doesn't work on ABCL."
-    (uiop:with-temporary-file (:stream s :pathname pn
-                               :type "tar.gz"
-                               :element-type '(unsigned-byte 8))
-      (tar:with-open-archive (a s :direction :output :type :v7)
-        (write-a.txt a)
-        (write-a-symlink.txt a)
-        (write-a-hardlink.txt a))
-      :close-stream
-      (tar:with-open-archive (a pn)
-        (let ((entry))
-          (setf entry (tar:read-entry a))
-          (read-a.txt :v7 entry)
+    (para:finish
+     (uiop:with-temporary-file (:stream s :pathname pn
+                                :type "tar.gz"
+                                :element-type '(unsigned-byte 8))
+       (tar:with-open-archive (a s :direction :output :type :v7)
+         (write-a.txt a)
+         (write-a-symlink.txt a)
+         (write-a-hardlink.txt a))
+       :close-stream
+       (tar:with-open-archive (a pn)
+         (let ((entry))
+           (setf entry (tar:read-entry a))
+           (read-a.txt :v7 entry)
 
-          ;; Next is the a-symlink.txt -> a.txt symlink
-          (setf entry (tar:read-entry a))
-          (read-a-symlink.txt :v7 entry)
+           ;; Next is the a-symlink.txt -> a.txt symlink
+           (setf entry (tar:read-entry a))
+           (read-a-symlink.txt :v7 entry)
 
-          ;; Next is the a-hardlink.txt -> a.txt hardlink
-          (setf entry (tar:read-entry a))
-          (read-a-hardlink.txt :v7 entry)
+           ;; Next is the a-hardlink.txt -> a.txt hardlink
+           (setf entry (tar:read-entry a))
+           (read-a-hardlink.txt :v7 entry)
 
-          ;; End of tar-file
-          (setf entry (tar:read-entry a))
-          (para:true (null entry)))))))
+           ;; End of tar-file
+           (setf entry (tar:read-entry a))
+           (para:true (null entry))))))))
