b3f860503b481b8f4cfff561bb216868e7c90289
Eric Timmons <etimmons@mit.edu>
2022-01-03 17:43:00 -0500

    Remove a use of uiop:define-package

--- src/asdf/package.lisp
+++ src/asdf/package.lisp
@@ -3,7 +3,7 @@
 ;;;; This software is part of cl-tar. See README.org for more information. See
 ;;;; LICENSE for license information.
 
-(uiop:define-package #:tar-cli-asdf
+(cl:defpackage #:tar-cli-asdf
     (:use #:cl)
   (:local-nicknames (#:ops #:asdf-release-ops))
   (:export #:tar-cli-system))
